38b7c208b50a8a0b731e82e627a246ff873c7ee8
Matteo Nastasi <nastasi@openquake.org>
2014-07-30 16:00:26 +0200

    working version for all fields read-only not ownership aware

--- example/example/admin.py
+++ example/example/admin.py
@@ -1,43 +1,86 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 from nested_inlines.admin import NestedModelAdmin, NestedTabularInline, NestedStackedInline
 
 from models import A, B, C
 
+class OwnerException(Exception):
+    pass
+
 class CInline(NestedTabularInline):
     model = C
-    exclude = ('owner',)
+    exclude = ('id', 'owner')
+    max_num = 1
+    extra = 0
 
+    def get_readonly_fields(self, request, obj=None):
+        # import pdb ; pdb.set_trace()
+        fieldnames = [x.name for x in self.model._meta.fields]
+        print "GET_READONLY_FIELDS C: ", obj
+
+        if hasattr(self, "exclude") and self.exclude is not None:
+            return tuple( set(fieldnames) - set(self.exclude) )
+        else:
+            return tuple(fieldnames)
 
 class BInline(NestedStackedInline):
     model = B
-    exclude = ('owner',)
+    exclude = ('id', 'owner')
     inlines = [CInline,]
+    max_num = 1
+    extra = 0
 
+    def get_readonly_fields(self, request, obj=None):
+        fieldnames = [x.name for x in self.model._meta.fields]
+        # import pdb ; pdb.set_trace()
+        print "GET_READONLY_FIELDS B: ", obj
+
+        if hasattr(self, "exclude") and self.exclude is not None:
+            return tuple( set(fieldnames) - set(self.exclude) )
+        else:
+            return tuple(fieldnames)
 
 class AAdmin(NestedModelAdmin):
-    exclude = ('owner',)
+    exclude = ('id', 'owner')
     inlines = [BInline,]
+    max_num = 1
+    extra = 0
 
-    def save_model(self, request, obj, form, change):
-        obj.owner_id = request.user.id
+    def save_model_off(self, request, obj, form, change):        
+        print "save_model override (A) change: ", ("TRUE" if change else "FALSE")
+        if change:
+            print "OWNED: ", ("YES" if obj.is_owned_by(request.user, include_superuser=True) else "NO")
+            if not obj.is_owned_by(request.user, include_superuser=True):
+                raise ValidationError("ototo")
 
+        if not change:
+            obj.owner_id = request.user.id
+        else:
+            if not obj.owner_id:
+                raise OwnerException("Inconsistent record %d, no owner id found" % request.pk)
+
+        # ugly but works
+        request._gem_custom = obj.owner_id
         super(AAdmin, self).save_model(request, obj, form, change)
 
 
-    def save_formset(self, request, form, formset, change):
+    def save_formset_off(self, request, form, formset, change):
         """
         Given an inline formset save it to the database.
         """
-        print "save_formset override (A)"
+        print "save_formset override (A) ", request._gem_custom
 
         instances = formset.save(commit=False)
         for instance in instances:
             print "instance: ", instance
-
-            instance.owner_id = request.user.id
+            # import pdb ; pdb.set_trace()
+            
+            if hasattr(instance, "owner_id"):
+                instance.owner_id = request._gem_custom
             instance.save()
+        # formset.save()
+        formset.save_m2m()
 
-        formset.save()
         # this save_m2m arrives from an official django example:
         # https://docs.djangoproject.com/en/1.5/ref/contrib/admin/#django.contrib.admin.ModelAdmin.save_formset
         # and is different from the save_formset defined in django_nested_inlines extension
@@ -54,5 +97,20 @@ class AAdmin(NestedModelAdmin):
                 for nested_formset in form.nested_formsets:
                     self.save_formset(request, form, nested_formset, change)
 
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        # import pdb ; pdb.set_trace()
+        fieldnames = [x.name for x in self.model._meta.fields]
+        print "GET_READONLY_FIELDS A: ", obj
+
+        if hasattr(self, "exclude") and self.exclude is not None:
+            return tuple( set(fieldnames) - set(self.exclude) )
+        else:
+            return tuple(fieldnames)
 
 admin.site.register(A, AAdmin)
--- example/example/models.py
+++ example/example/models.py
@@ -2,22 +2,22 @@ from django.db import models
 from django_extras.contrib.auth.models import SingleOwnerMixin
 
 class A(SingleOwnerMixin, models.Model):
-    name = models.CharField("name", max_length=255)
+    name_a = models.CharField("name", max_length=255)
 
     def __unicode__(self):
-        return self.name
+        return self.name_a
 
 class B(SingleOwnerMixin, models.Model):
     a = models.ForeignKey(A)
-    name = models.CharField("name", max_length=255)
+    name_b = models.CharField("name", max_length=255)
 
     def __unicode__(self):
-        return self.name
+        return self.name_b
     
 class C(SingleOwnerMixin, models.Model):
     b = models.ForeignKey(B)
-    name = models.CharField("name", max_length=255)
+    name_c = models.CharField("name", max_length=255)
 
     def __unicode__(self):
-        return self.name
+        return self.name_c
 
